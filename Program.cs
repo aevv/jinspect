27128b8c1d9fcbec1b24deb78ad16959f2e3f10a
Matt Thompson <matt@aevv.net>
2026-05-10 14:15:19 +0100

    add --version

--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
+using System.Reflection;
 using JInspect.Commands;
 using Spectre.Console.Cli;
+
+var version = Assembly.GetExecutingAssembly()
+    .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+    ?? Assembly.GetExecutingAssembly().GetName().Version?.ToString()
+    ?? "unknown";
+
+var plusIndex = version.IndexOf('+');
+if (plusIndex >= 0) version = version[..plusIndex];
 
 var app = new CommandApp<InspectCommand>();
 
 app.Configure(config =>
 {
     config.SetApplicationName("jinspect");
+    config.SetApplicationVersion(version);
 });
 
 return app.Run(args);
